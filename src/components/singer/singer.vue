95be76eb683c6f11197891bf22cf3f4d3a0c4a52
lethe <978414728@qq.com>
2018-01-24 09:31:47 +0800

    init

---
+++ src/components/singer/singer.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    歌手页面
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+</script>
+
+<style scoped lang="stylus" rel="stylussheet/stylus">
+  @import '~common/stylus/variable'
+</style>
